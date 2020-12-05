127dc119facd034b044216885728419f2f800c13
bitchboy420 <75518600+bitchboy420@users.noreply.github.com>
2020-12-05 00:56:24 -0600

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+
+  "prefix": "^"
+
+  "ownerOnly": false
+
+  "ownerID": ""
+
+  "channelName": "ping"
+
+  "pingInterval": "100"
+
+  "token": ""
+
+}
+
+module.exports = config;
